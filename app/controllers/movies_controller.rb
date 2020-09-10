f1e8ed77c7bd71653a47cbac95cce21b408e0a3b
Nico Proto <nicolasproto@gmail.com>
2020-09-10 10:49:53 +1000

    Search lecture

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,21 @@
+class MoviesController < ApplicationController
+  def index
+    if params[:query].present?
+      # sql_query = " \
+      #   movies.title @@ :query \
+      #   OR movies.syllabus @@ :query \
+      #   OR directors.first_name @@ :query \
+      #   OR directors.last_name @@ :query
+      # "
+      # @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
+
+      # @movies = Movie.search_by_title_and_syllabus(params[:query])
+
+      # @movies = PgSearch.multisearch(params[:query]).map { |pgdoc| pgdoc.searchable }
+
+      @movies = Movie.search(params[:query])
+    else
+      @movies = Movie.all
+    end
+  end
+end
---
+++ app/models/movie.rb
@@ -0,0 +1,19 @@
+class Movie < ApplicationRecord
+  belongs_to :director
+
+  searchkick
+
+  include PgSearch::Model
+  multisearchable against: [:title, :syllabus]
+
+                    # name of the method we will call
+  pg_search_scope :search_by_title_and_syllabus,
+    against: [ :title, :syllabus ],
+    associated_against: {
+      director: [ :first_name, :last_name ]
+    },
+    using: {
+      # allows partial words
+      tsearch: { prefix: true }
+    }
+end
